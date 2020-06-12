f406bdc5cc3c67605cc69f747302767e9a0d206d
unknown <dnicerio@gmail.com>
2020-06-13 00:02:47 +0800

    feat: added animations to Contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { motion } from "framer-motion"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -10,21 +11,21 @@ const ContactPage = () => (
     <SEO title="Contact" />
     <div className="contact-section">
       <div className="contact-container">
-        <div className="contact-header">
+        <motion.div initial={{ y: -1440 }} animate={{ y: 0 }} className="contact-header">
           <h2>
             I'm available, <span>let's work together</span>.
           </h2>
-        </div>
+        </motion.div>
         <div className="contact-icon">
-            <a href="https://www.linkedin.com/in/darrylnicerio/" className="icon" target="_blank" rel="noreferrer">
+            <motion.a initial={{ x: -1440 }} animate={{ x: 0 }} href="https://www.linkedin.com/in/darrylnicerio/" className="icon" target="_blank" rel="noreferrer">
               <i className="fa fa-linkedin" aria-hidden="true"></i>                
-            </a>
-            <a href="https://github.com/dnicerio/" className="icon" target="_blank" rel="noreferrer">
+            </motion.a>
+            <motion.a initial={{ y: 1440 }} animate={{ y: 0 }} href="https://github.com/dnicerio/" className="icon" target="_blank" rel="noreferrer">
               <i className="fa fa-github" aria-hidden="true"></i>                
-            </a>
-            <a href="mailto:dnicerio@gmail.com" className="icon" target="_blank" rel="noreferrer">
+            </motion.a>
+            <motion.a initial={{ x: 1440 }} animate={{ x: 0 }} href="mailto:dnicerio@gmail.com" className="icon" target="_blank" rel="noreferrer">
             <i className="fa fa-envelope" aria-hidden="true"></i>                
-            </a>
+            </motion.a>
         </div>
       </div>
     </div>
