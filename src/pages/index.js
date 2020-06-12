f31d9928922f6c62442d31bf2547c7729ab4af5f
unknown <dnicerio@gmail.com>
2020-06-12 21:47:07 +0800

    feat: added animations to headers in Home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { motion } from "framer-motion"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -10,8 +11,12 @@ const IndexPage = () => (
     <SEO title="Home" />
     <div className="index-section">
       <div className="header-container">
-        <h1 className="main-header">Hi! I'm <span>Darryl Nicerio</span>.</h1>
-        <p className="sub-header">a <span>front-end</span> web developer.</p>
+        <motion.h1 initial={{ x: -1200 }} animate={{ x: 0 }} className="main-header">
+          Hi! I'm <span>Darryl Nicerio</span>.
+        </motion.h1>
+        <motion.p initial={{ x: 1200 }} animate={{ x: 0 }} className="sub-header">
+          a <span>front-end</span> web developer.
+        </motion.p>
       </div>
     </div>
   </Layout>
